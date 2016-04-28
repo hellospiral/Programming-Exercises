bc3bf350cb51f2f7772fd94701f40b1f3294baf3
hellospiral <matthewthomascarlson@gmail.com>
2016-04-27 18:38:01 -0700

    add solution to where art thou

--- intermediate_scripting.js
+++ intermediate_scripting.js
@@ -1,4 +1,53 @@
 /*
+Where art thou
+
+Make a function that looks through an array of objects (first argument) and returns an array of all objects that have matching property and value pairs (second argument). Each property and value pair of the source object has to be present in the object from the collection if it is to be included in the returned array.
+
+For example, if the first argument is [{ first: "Romeo", last: "Montague" }, { first: "Mercutio", last: null }, { first: "Tybalt", last: "Capulet" }], and the second argument is { last: "Capulet" }, then you must return the third object from the array (the first argument), because it contains the property and its value, that was passed on as the second argument.
+
+tests: 
+
+whereAreYou([{ first: "Romeo", last: "Montague" }, { first: "Mercutio", last: null }, { first: "Tybalt", last: "Capulet" }], { last: "Capulet" }) 
+should return [{ first: "Tybalt", last: "Capulet" }]
+
+whereAreYou([{ "a": 1 }, { "a": 1 }, { "a": 1, "b": 2 }], { "a": 1 }) 
+should return [{ "a": 1 }, { "a": 1 }, { "a": 1, "b": 2 }]
+
+whereAreYou([{ "a": 1, "b": 2 }, { "a": 1 }, { "a": 1, "b": 2, "c": 2 }], { "a": 1, "b": 2 })
+should return [{ "a": 1, "b": 2 }, { "a": 1, "b": 2, "c": 2 }]
+
+whereAreYou([{ "a": 1, "b": 2 }, { "a": 1 }, { "a": 1, "b": 2, "c": 2 }], { "a": 1, "c": 2 }) 
+should return [{ "a": 1, "b": 2, "c": 2 }]
+*/
+
+function whereAreYou(collection, source) {
+  var arr = [];
+  var sourcekeys = Object.keys(source);
+  
+  if (sourcekeys.length === 1) {
+    var sourceProp = sourcekeys[0];
+    for (var i = 0; i < collection.length; i++) {
+      if ((collection[i].hasOwnProperty(sourcekeys[0])) && (collection[i][sourceProp] === source[sourceProp])) {
+        arr.push(collection[i]);
+      }
+    }
+  }
+  else if (sourcekeys.length === 2) {
+    var sourceProp1 = sourcekeys[0];
+    var sourceProp2 = sourcekeys[1];
+    for (var i = 0; i < collection.length; i++) {
+      if ((collection[i].hasOwnProperty(sourcekeys[0])) && (collection[i].hasOwnProperty(sourcekeys[1]))) {
+        if (collection[i][sourceProp1] === source[sourceProp1] && collection[i][sourceProp2] === source[sourceProp2]) {
+          arr.push(collection[i]);
+        }  
+      }
+    }
+  }
+  
+  return arr;
+}
+
+/*
 Convert to Roman
 
 Convert the given number into a roman numeral.
