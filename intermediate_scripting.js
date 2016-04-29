6540ec34b67bcebd03fe4a633fe77bc84276aad6
hellospiral <matthewthomascarlson@gmail.com>
2016-04-28 19:03:51 -0700

    add Search and Replace

--- intermediate_scripting.js
+++ intermediate_scripting.js
@@ -1,23 +1,38 @@
 /*
+Search and Replace
+
+Perform a search and replace on the sentence using the arguments provided and return the new sentence.
+
+First argument is the sentence to perform the search and replace on.
+
+Second argument is the word that you will be replacing (before).
+
+Third argument is what you will be replacing the second argument with (after).
+
+NOTE: Preserve the case of the original word when you are replacing it. For example if you mean to replace the word "Book" with the word "dog", it should be replaced as "Dog"
+*/
+function myReplace(str, before, after) {
+  if (before.charCodeAt(0) >= 65 && before.charCodeAt(0) <= 90) {
+    after = after.charAt(0).toUpperCase() + after.substring(1);
+  }
+  var strArray = str.split(" ");
+  for (var i = 0; i < strArray.length; i++) {
+  if (strArray[i] === before) {
+      strArray[i] = after;
+    }
+  }
+  str = strArray.join(' ');
+  return str;
+}
+
+myReplace("His name is Tom", "Tom", "john");
+
+/*
 Where art thou
 
 Make a function that looks through an array of objects (first argument) and returns an array of all objects that have matching property and value pairs (second argument). Each property and value pair of the source object has to be present in the object from the collection if it is to be included in the returned array.
 
 For example, if the first argument is [{ first: "Romeo", last: "Montague" }, { first: "Mercutio", last: null }, { first: "Tybalt", last: "Capulet" }], and the second argument is { last: "Capulet" }, then you must return the third object from the array (the first argument), because it contains the property and its value, that was passed on as the second argument.
-
-tests: 
-
-whereAreYou([{ first: "Romeo", last: "Montague" }, { first: "Mercutio", last: null }, { first: "Tybalt", last: "Capulet" }], { last: "Capulet" }) 
-should return [{ first: "Tybalt", last: "Capulet" }]
-
-whereAreYou([{ "a": 1 }, { "a": 1 }, { "a": 1, "b": 2 }], { "a": 1 }) 
-should return [{ "a": 1 }, { "a": 1 }, { "a": 1, "b": 2 }]
-
-whereAreYou([{ "a": 1, "b": 2 }, { "a": 1 }, { "a": 1, "b": 2, "c": 2 }], { "a": 1, "b": 2 })
-should return [{ "a": 1, "b": 2 }, { "a": 1, "b": 2, "c": 2 }]
-
-whereAreYou([{ "a": 1, "b": 2 }, { "a": 1 }, { "a": 1, "b": 2, "c": 2 }], { "a": 1, "c": 2 }) 
-should return [{ "a": 1, "b": 2, "c": 2 }]
 */
 
 function whereAreYou(collection, source) {
