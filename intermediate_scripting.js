beedf661bc3363aa6d2c82179c25ee7f083d8e7f
hellospiral <matthewthomascarlson@gmail.com>
2016-05-09 18:40:35 -0700

    add pairElement function to intermediate_scripting.js

--- intermediate_scripting.js
+++ intermediate_scripting.js
@@ -1,4 +1,39 @@
 /*
+DNA Pairing
+The DNA strand is missing the pairing element. Take each character, get its pair, and return the results as a 2d array.
+
+Base pairs are a pair of AT and CG. Match the missing element to the provided character.
+
+Return the provided character as the first element in each array.
+
+For example, for the input GCG, return [["G", "C"], ["C","G"],["G", "C"]]
+
+The character and its pair are paired up in an array, and all the arrays are grouped into one encapsulating array.
+*/
+function pairElement(str) {
+  var strArray = str.split("");
+  var newArray = [];
+  for (var i = 0; i < strArray.length; i++) {
+    if (strArray[i] === "G") {
+      newArray.push(["G", "C"]);
+    }
+    else if (strArray[i] === "C") {
+      newArray.push(["C", "G"]);
+    }
+    else if (strArray[i] === "A") {
+      newArray.push(["A", "T"]);
+    }
+    else if (strArray[i] === "T") {
+      newArray.push(["T", "A"]);
+    }
+  }
+  return newArray;
+}
+
+pairElement("GCG");
+
+
+/*
 Pig Latin
 Translate the provided string to pig latin.
 
